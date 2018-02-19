db91601e7664b60669110305d75c6421ee602c7f
Ricardo Delfin Garcia <ricardo.delfin.garcia@gmail.com>
2018-02-19 16:04:32 -0600

    Added image cache to person_finder

--- golem_campose/src/person_finder.cpp
+++ golem_campose/src/person_finder.cpp
@@ -1,25 +1,64 @@
+#include <mutex>
+#include <queue>
+#include <unordered_map>
+
 #include <ros/ros.h>
 
 #include <campose_msgs/FramePoses.h>
 #include <campose_msgs/PersonAngles.h>
 
 #include <std_msgs/Float64MultiArray.h>
+#include <std_msgs/ColorRGBA.h>
+#include <sensor_msgs/Image.h>
 
 #define CONFIDENCE_THRESHOLD (0.1)
 #define POS_TO_THETA         (1)
+#define QUEUE_SIZE           (1000)
 
+std::mutex image_map_lock;
+std::unordered_map<int, sensor_msgs::Image> image_map;
+std::queue<int> image_queue;
+
+uint64_t rostime_to_long(const ros::Time& time) {
+    return ((uint64_t)time.sec)*1000000 + (uint64_t)time.nsec;
+}
 
 ros::Publisher person_publisher;
 
 double pose_to_angle(const campose_msgs::PersonPose& person_pose);
+bool get_color(const std_msgs::ColorRGBA& color, campose_msgs::Keypoint torso);
+
+void image_cb(const sensor_msgs::Image::ConstPtr& msg) {
+    uint64_t key;
+
+    image_map_lock.lock();
+
+    if(image_queue.size() != image_map.size())
+        ROS_WARN("Person finder image queue and map are not the same size. Map has %lu elements and que has %lu elements.", image_map.size(), image_queue.size());
+
+    while(image_queue.size() > QUEUE_SIZE) {
+        key = image_queue.front();
+        image_map.erase(key);
+        image_queue.pop();
+    }
+
+    key = rostime_to_long(msg->header.stamp);
+    image_queue.push(key);
+    image_map[key] = *msg;
+
+    image_map_lock.unlock();
+}
 
 void keypointCallback(const campose_msgs::FramePoses::ConstPtr& msg) {
     campose_msgs::PersonAngles people_angles;
     
     people_angles.header = msg->header;
 
-    for(const campose_msgs::PersonPose& person_pose : msg->poses)
-        people_angles.angles.push_back(pose_to_angle(person_pose));
+    for(const campose_msgs::PersonPose& person_pose : msg->poses) {
+        people_angles.people.emplace_back();
+        size_t idx = people_angles.people.size() - 1;
+        people_angles.people[idx].angle = pose_to_angle(person_pose);
+    }
 
     person_publisher.publish(people_angles);
 }
@@ -28,12 +67,20 @@ int main(int argc, char* argv[]) {
     ros::init(argc, argv, "person_finder");
     ros::NodeHandle nh;
 
+    std::string camera_topic;
+    nh.param<std::string>("camera_topic", camera_topic, "/flycap_cam/image");
+
     person_publisher = nh.advertise<campose_msgs::PersonAngles>("people_angles", 1000);
     ros::Subscriber keypoint_subscriber = nh.subscribe("person_keypoints", 1000, keypointCallback);
+    ros::Subscriber img_subscriber = nh.subscribe(camera_topic, 1000, image_cb);
 
     ROS_INFO("Converting keypoints to angles...");
 
     ros::spin();
+}
+
+bool get_color(const std_msgs::ColorRGBA& color, campose_msgs::Keypoint torso) {
+
 }
 
 double pose_to_angle(const campose_msgs::PersonPose& person_pose) {
