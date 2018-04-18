181f84754360003ce7f3a966712b2408aff61c8d
Ricardo Delfin Garcia <ricardo.delfin.garcia@gmail.com>
2018-04-17 21:15:27 -0500

    Partial commit with some of the kinect protocol implemented

--- golem_campose/src/kinect_server.cpp
+++ golem_campose/src/kinect_server.cpp
@@ -2,6 +2,7 @@
 #include <mutex>
 
 #include <golem_campose/kinect_server.hpp>
+#include <golem_campose/pc_serializer.hpp>
 
 #include <ros/ros.h>
 
@@ -85,26 +86,92 @@ KinectServer::~KinectServer() {
     }
 }
 
-void KinectServer::client_recv(int sockfd, uint64_t id) {
-    char* buffer = (char *)malloc(sizeof(char)*256);
+#define BUFFER_SIZE 1024
 
-    if (sockfd < 0)
+void KinectServer::client_recv(int sockfd, uint64_t id) {
+    char* buffer = (char *)malloc(sizeof(char)*BUFFER_SIZE);
+    PcSerializer pcs;
+    std::vector<uint8_t> v_buffer;
+    v_buffer.reserve(BUFFER_SIZE);
+
+    if (sockfd < 0) {
         ROS_ERROR("Socket %lu\tERROR on accept", id);
-    bzero(buffer,256);
+        close(sockfd);
+        free(buffer);
+        return;
+    }
+
+    bzero(buffer,BUFFER_SIZE);
 
     // Handshake
-    int n = read(sockfd,buffer,255);
-    if(n < 0)
-        ROS_ERROR("Socket %lu\tERROR reading", id);
+    int n = read(sockfd,buffer,BUFFER_SIZE - 1);
+    if(n < 0) {
+        ROS_ERROR("Socket %lu\tERROR reading handshake", id);
+        close(sockfd);
+        free(buffer);
+        return;
+    }
 
     if(strlen(buffer) == HANDSHAKE_MSG.length() &&
        strncmp(HANDSHAKE_MSG.c_str(), buffer, HANDSHAKE_MSG.length()) == 0)
 
     n = write(sockfd, HANDSHAKE_MSG.c_str(), HANDSHAKE_MSG.length());
-    if(n < 0)
+    if(n < 0) {
         ROS_ERROR("Socket %lu\tERROR writing to socket", id);
+        close(sockfd);
+        free(buffer);
+        return;
+    }
 
     ROS_INFO("Socket %lu\tSuccessfully finished handshake", id);
+
+    bool running = true;
+    bool parsing = false;
+
+    while(running) {
+        memset(buffer, 0, BUFFER_SIZE);
+        n = read(sockfd, buffer, BUFFER_SIZE - 1);
+
+        if(n < 0) {
+            ROS_ERROR("Socket %lu\tERROR reading socket", id);
+            close(sockfd);
+            free(buffer);
+        } if(n > 0) {
+            uint8_t* ptr = &v_buffer[v_buffer.size()];
+            v_buffer.resize(v_buffer.size()+n);
+            memcpy(ptr, buffer, n);
+        }
+
+        if(!parsing) {
+            bool header_ok = false;
+            while(!header_ok && v_buffer.size() >= 4) {
+                if(memcmp(&v_buffer[0], "snd", 3) == 0) {
+                    header_ok = true;
+                    // Remove header
+                    parsing = true;
+                    v_buffer.erase(v_buffer.begin(), v_buffer.begin() + 4);
+
+                } else if(memcmp(&v_buffer[0], "end", 3) == 0) {
+                    header_ok = true;
+                    // You can close the socket now.
+                    running = false;
+                } else {
+                    // Invalid header. Skip to next char
+                    v_buffer.erase(v_buffer.begin());
+                }
+            }
+        }
+
+        // value of parsing may change during the last if
+        if(parsing) {
+            uint64_t msg_size;
+            // Get header size
+            if(pcs.kinect_frame_size(&v_buffer[4], v_buffer.size(), msg_size) && v_buffer >= msg_size) {
+                
+            }
+        }
+
+    }
 
     close(sockfd);
     free(buffer);
